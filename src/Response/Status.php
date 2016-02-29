f9bd98eeb27a7aa110cfc86af57c26cdee9c031b
Roman3349 <ondracek.roman@centrum.cz>
2016-02-29 21:22:21 +0100

    Rewrite function msgToCode

--- src/Response/Status.php
+++ src/Response/Status.php
@@ -4,7 +4,7 @@ namespace ITManie\IQRF\Response;
 
 class Status {
 
-	public $array = ['OK' => 'OK',
+	private $array = ['OK' => 'OK',
 		'ERROR 1' => 'Data string exceeds 64B.',
 		'ERROR 2' => 'Unfilled required fields. Required parameter(s) missing.',
 		'ERROR 3' => 'No data sent. Writing error, data has not been written.',
@@ -43,47 +43,10 @@ class Status {
 	 * @return string Status code
 	 */
 	public function msgToCode($message) {
-		switch ($message) {
-			case 'OK':
-				return 'OK';
-			case 'Data string exceeds 64B.':
-				return 'ERROR 1';
-			case 'Unfilled required fields. Required parameter(s) missing.':
-				return 'ERROR 2';
-			case 'No data sent. Writing error, data has not been written.':
-				return 'ERROR 3';
-			case 'Data access denied. The user has no access to given GW.':
-				return 'ERROR 4';
-			case 'Incorrect username or password.':
-				return 'ERROR 5';
-			case 'No data for specified request.':
-				return 'ERROR 6';
-			case 'Error issued by MySQL server when attempting to write to the database.':
-				return 'ERROR 7';
-			case 'Incorrectly completed parameter new.':
-				return 'ERROR 8';
-			case 'Incorrect data format. Every byte of binary data must be converted to 2 bytes corresponding to its hexadecimal value in ASCII representation.':
-				return 'ERROR 9';
-			case 'Device is already assigned.':
-				return 'ERROR 10';
-			case 'Incorrect password to GW.':
-				return 'ERROR 11';
-			case 'Incorrect user.':
-				return 'ERROR 12';
-			case 'The device can not be changed. Device is not assigned to a given user account.':
-				return 'ERROR 13';
-			case 'The device can not be changed. Incorrect password.':
-				return 'ERROR 14';
-			case 'The device can not be added.':
-				return 'ERROR 15';
-			case 'Maximum count of GW is reached according to user\'s license.':
-				return 'ERROR 16';
-			case 'User does not have API key.':
-				return 'ERROR 17';
-			case 'Incorrect signature.':
-				return 'ERROR 18';
-			default:
-				return 'Invalid status code.';
+		if (array_search($message, $this->array, true)) {
+			return array_keys($this->array, $message, true)[0];
+		} else {
+			return 'Invalid status code.';
 		}
 	}
 
