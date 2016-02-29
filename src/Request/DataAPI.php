413f265ea3c0aecec43b0d0e0b2caafdb59f367a
Roman3349 <ondracek.roman@centrum.cz>
2016-02-29 20:11:31 +0100

    Added funtion to get new data and send data via API

--- src/Request/DataAPI.php
+++ src/Request/DataAPI.php
@@ -12,10 +12,26 @@ class DataAPI {
 	/**
 	 * Get lasted data from the Cloud (incoming from the API)
 	 * @param int $gatewayID ID of gateway
+	 * @param int $count Count of lasted messages
 	 * @return string $response Response of request
 	 */
-	public function getLasted($gatewayID) {
-		$parameter = 'ver=' . IQRF::API_VER . '&uid=' . Config::getUserID() . '&gid=' . $gatewayID . '&cmd=dnlc&last=1';
+	public function getLast($gatewayID, $count = 1) {
+		$parameter = 'ver=' . IQRF::API_VER . '&uid=' . Config::getUserID() . '&gid=' . $gatewayID . '&cmd=dnlc&last=1&count=' . $count;
+		$signature = Utils::createSignature($parameter, Config::getApiKey());
+		$parameter += '&signature=' . $signature;
+		$client = new Client(['base_uri' => IQRF::API_URI]);
+		$response = $client->request('GET', $parameter);
+		return $response;
+	}
+	
+	/**
+	 * Get new data from the Cloud (incoming from the API)
+	 * @param int $gatewayID ID of gateway
+	 * @param int $count Count of lasted messages
+	 * @return string $response Response of request
+	 */
+	public function getNew($gatewayID, $count = 1) {
+		$parameter = 'ver=' . IQRF::API_VER . '&uid=' . Config::getUserID() . '&gid=' . $gatewayID . '&cmd=dnlc&new=1&count=' . $count;
 		$signature = Utils::createSignature($parameter, Config::getApiKey());
 		$parameter += '&signature=' . $signature;
 		$client = new Client(['base_uri' => IQRF::API_URI]);
@@ -23,4 +39,19 @@ class DataAPI {
 		return $response;
 	}
 
+
+	/**
+	 * Send data to IQRF Cloud
+	 * @param int $gatewayID ID of gateway
+	 * @param mixed $data Data
+	 * @return string $response Response of request
+	 */
+	public function send($gatewayID, $data) {
+		$parameter = 'ver=' . IQRF::API_VER . '&uid=' . Config::getUserID() . '&gid=' . $gatewayID . '&cmd=uplc&data=' . $data;
+		$signature = Utils::createSignature($parameter, Config::getApiKey());
+		$parameter += '&signature=' . $signature;
+		$client = new Client(['base_uri' => IQRF::API_URI]);
+		$response = $client->request('GET', $parameter);
+		return $response;
+	}
 }
--- src/Request/DataGW.php
+++ src/Request/DataGW.php
@@ -12,10 +12,26 @@ class DataGW {
 	/**
 	 * Get lasted data from the Cloud (incoming from the GW)
 	 * @param int $gatewayID ID of gateway
+	 * @param int $count Count of lasted messages
 	 * @return string $response Response of request
 	 */
-	public function getLasted($gatewayID) {
-		$parameter = 'ver=' . IQRF::API_VER . '&uid=' . Config::getUserID() . '&gid=' . $gatewayID . '&cmd=dnld&last=1';
+	public function getLast($gatewayID, $count = 1) {
+		$parameter = 'ver=' . IQRF::API_VER . '&uid=' . Config::getUserID() . '&gid=' . $gatewayID . '&cmd=dnld&last=1&count=' . $count;
+		$signature = Utils::createSignature($parameter, Config::getApiKey());
+		$parameter += '&signature=' . $signature;
+		$client = new Client(['base_uri' => IQRF::API_URI]);
+		$response = $client->request('GET', $parameter);
+		return $response;
+	}
+	
+	/**
+	 * Get new data from the Cloud (incoming from the GW)
+	 * @param int $gatewayID ID of gateway
+	 * @param int $count Count of lasted messages
+	 * @return string $response Response of request
+	 */
+	public function getNew($gatewayID, $count = 1) {
+		$parameter = 'ver=' . IQRF::API_VER . '&uid=' . Config::getUserID() . '&gid=' . $gatewayID . '&cmd=dnld&new=1&count=' . $count;
 		$signature = Utils::createSignature($parameter, Config::getApiKey());
 		$parameter += '&signature=' . $signature;
 		$client = new Client(['base_uri' => IQRF::API_URI]);
